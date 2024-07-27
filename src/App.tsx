35b24b8bbc9802d4deac2465fe3c659d2a872a96
Jake <jakehuntlyrobinson@gmail.com>
2024-07-27 23:56:02 +0100

    Finally fix google OAuth login, working as expected

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Navigate to="/admin" />} />
+        <Route path="/" element={<UserScreen/>} />
         <Route path="/admin" element={<AdminScreen />} />
         <Route path="/home" element={<UserScreen />} />
         <Route path="/signup" element={<Signup />} />
