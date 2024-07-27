929676a7a6073cdf9114aafc3c225a9cc57a5ff3
Jake <jakehuntlyrobinson@gmail.com>
2024-07-27 23:34:06 +0100

    remove early redirect

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,8 +1,8 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 const supabaseClient: SupabaseClient = createClient(
-    'https://tdeiarvjcfnwlwbxzadr.supabase.co',
-    import.meta.env.VITE_SUPABASE_KEY
+  "https://tdeiarvjcfnwlwbxzadr.supabase.co",
+  import.meta.env.VITE_SUPABASE_KEY
 );
 
 export default supabaseClient;
