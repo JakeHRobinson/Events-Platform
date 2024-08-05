c777d4178db3653230aa67166d224f46d1e32a0b
Jake <jakehuntlyrobinson@gmail.com>
2024-08-05 03:28:21 +0100

    remove now unused navbar styling file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,8 @@ import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
 import { useNavigate, useLocation } from "react-router-dom";
 import getSession from "../utils/getSession";
-import "./navbarStyling.css";
 import getUser from "../utils/getUser";
 
 interface User {
